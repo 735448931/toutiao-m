800615024c9fac53f3b330bd0747425f422740cb
haoxuan <735448931@qq.com>
2022-11-06 09:32:50 +0800

    项目初始化-引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <h1>hahah</h1>
+    <h1>引入vant组件库</h1>
     <!-- 路由的出口 -->
     <router-view />
   </div>
 </template>
-
 <script>
 export default {
   name: "App",
 };
 </script>
+
 
 <style lang="less">
 </style>
